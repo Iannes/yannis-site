ee3587798f6c96f8d588f479d0f2b1199acd1073
Yannis Spyrou <yannispyrou@gmail.com>
2022-10-17 20:09:12 +0200

    comment out emoji

--- src/components/modules/Intro/index.tsx
+++ src/components/modules/Intro/index.tsx
@@ -7,9 +7,9 @@ const Intro = () => (
     <Header />
     <div className="container py-16 flex items-center flex-col md:flex-row justify-between">
       <div className="flex-1 w-full md:w-1/2 mb-8 md:mb-0">
-        <h1 className="mb-8 text-3xl md:text-5xl font-bold text-brand-primary dark:text-white typography">
-          Hi there 👋
-        </h1>
+        <h1 className="mb-8 text-3xl md:text-5xl font-bold text-brand-primary dark:text-white typography">Hi there</h1>
+        Hi there
+        {/* <span className="md:visible sm:invisible">👋</span> */}
         <h2 className="mb-[2.5rem] text-2xl md:text-4xl text-brand-secondary dark:text-slate-200 typography">
           I’m Yannis and I’m a frontend engineer!
         </h2>
