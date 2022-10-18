346c80f4976ffb5e26eda766cbae41f04c867154
Yannis Spyrou <yannispyrou@gmail.com>
2022-10-18 19:23:56 +0200

    add a11y

--- src/components/ui/theme/Header/Hamburger/index.tsx
+++ src/components/ui/theme/Header/Hamburger/index.tsx
@@ -5,29 +5,33 @@ type HamburgerProps = {
   toggle: (value: boolean) => void;
 };
 
-const Hamburger = ({ sidebar, toggle }: HamburgerProps) => (
-  <div
-    className={cx('z-20 top-[1.6rem] right-[1.8rem] cursor-pointer burger-transition absolute block lg:hidden', {
-      'right-[66%] md:right-[35%] lg:fixed lg:right-[18%] top-[1.4rem]': sidebar,
-    })}
-    onClick={() => toggle(!sidebar)}
-  >
-    <div
-      className={cx('w-6 h-0.5 mb-1.5 burger-bar-transition bg-brand-primary', {
-        'translate-y-2 -rotate-[135deg] dark:bg-white': sidebar,
+const Hamburger = ({ sidebar, toggle }: HamburgerProps) => {
+  const handleToggle = () => toggle(!sidebar);
+  return (
+    <button
+      tabIndex={0}
+      className={cx('z-20 top-[1.6rem] right-[1.8rem] cursor-pointer burger-transition absolute block lg:hidden', {
+        'right-[8%] md:right-[35%] lg:fixed lg:right-[18%] top-[1.5rem]': sidebar,
       })}
-    />
-    <div
-      className={cx('w-6 h-0.5 mb-1.5 bg-brand-primary burger-bar-transition', {
-        'scale-0': sidebar,
-      })}
-    />
-    <div
-      className={cx('w-6 h-0.5 mb-1.5 bg-brand-primary burger-bar-transition', {
-        'bg-brand-primary dark:bg-white -translate-y-[7px] -rotate-45': sidebar,
-      })}
-    />
-  </div>
-);
+      onClick={handleToggle}
+    >
+      <div
+        className={cx('w-6 h-0.5 mb-1.5 burger-bar-transition bg-brand-primary', {
+          'translate-y-2 -rotate-[135deg] dark:bg-white': sidebar,
+        })}
+      />
+      <div
+        className={cx('w-6 h-0.5 mb-1.5 bg-brand-primary burger-bar-transition', {
+          'scale-0': sidebar,
+        })}
+      />
+      <div
+        className={cx('w-6 h-0.5 mb-1.5 bg-brand-primary burger-bar-transition', {
+          'bg-brand-primary dark:bg-white -translate-y-[7px] -rotate-45': sidebar,
+        })}
+      />
+    </button>
+  );
+};
 
 export default Hamburger;
