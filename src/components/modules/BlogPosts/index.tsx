5ad8e7cda6f7b0b8a151a66a612391024374b5a6
Yannis Spyrou <yannispyrou@gmail.com>
2022-10-22 18:26:12 +0200

    add blogposts section

---
+++ src/components/modules/BlogPosts/index.tsx
@@ -0,0 +1,18 @@
+import { BlogPost } from 'pages/blog';
+import Card from '../Card';
+
+type BlogPostsProps = { data: BlogPost[] };
+
+const BlogPosts: React.FC<BlogPostsProps> = ({ data }) => (
+  <div className="container py-8" id="blog">
+    <h2 className="text-2xl font-bold mb-8">Blog</h2>
+    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-span-1 md:col-span-2 lg:col-span-3">
+      {data?.map((post: BlogPost) => {
+        if (!post) return null;
+        return <Card key={post.content} data={post} />;
+      })}
+    </div>
+  </div>
+);
+
+export default BlogPosts;
