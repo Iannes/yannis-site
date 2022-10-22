171808901b7f961539a128ba799a15d91cef8d53
Yannis Spyrou <yannispyrou@gmail.com>
2022-10-22 20:42:43 +0200

    add underline

--- src/components/modules/BlogPosts/index.tsx
+++ src/components/modules/BlogPosts/index.tsx
@@ -5,7 +5,7 @@ type BlogPostsProps = { data: BlogPost[] };
 
 const BlogPosts: React.FC<BlogPostsProps> = ({ data }) => (
   <div className="container py-8" id="blog">
-    <h2 className="text-2xl md:text-4xl font-bold mb-8">Blog</h2>
+    <h2 className=" underline underline-offset-8text-2xl md:text-4xl font-bold mb-8">Blog</h2>
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-span-1 md:col-span-2 lg:col-span-3">
       {data?.map((post: BlogPost) => {
         if (!post) return null;
--- src/components/modules/Contact/index.tsx
+++ src/components/modules/Contact/index.tsx
@@ -4,6 +4,7 @@ import ContactForm from 'components/modules/Contact/ContactForm';
 const Contact = () => (
   <div className="container py-16 flex items-start justify-between flex-col lg:flex-row" id="contact">
     <div className="flex-none w-full lg:w-auto lg:flex-1 pr-0 lg:pr-8 order-2 lg:order-1">
+      <h2 className="underline underline-offset-8 text-2xl md:text-4xl font-bold mb-8">Contact me</h2>
       <ContactForm />
     </div>
     <div className="flex-none w-full lg:w-auto lg:flex-1 order-1 lg:order-2 mb-8 lg:mb-0">
