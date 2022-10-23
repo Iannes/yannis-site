f7cc1b9405426eeaeee8f4dcbfec5818b8550aa5
Yannis Spyrou <yannispyrou@gmail.com>
2022-10-23 17:27:35 +0200

    underline headings

--- src/components/modules/BlogPosts/index.tsx
+++ src/components/modules/BlogPosts/index.tsx
@@ -5,7 +5,8 @@ type BlogPostsProps = { data: BlogPost[] };
 
 const BlogPosts: React.FC<BlogPostsProps> = ({ data }) => (
   <div className="container py-8" id="blog">
-    <h2 className="underline underline-offset-8 text-2xl md:text-4xl font-bold mb-8">Blog</h2>
+    <h2 className="text-2xl md:text-4xl font-bold mb-1">Recent Posts</h2>
+    <span className="bg-black inline-block h-0.5 w-20 rounded bg-accent mb-8" />
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-span-1 md:col-span-2 lg:col-span-3">
       {data?.map((post: BlogPost) => {
         if (!post) return null;
--- src/components/modules/Contact/index.tsx
+++ src/components/modules/Contact/index.tsx
@@ -4,7 +4,8 @@ import ContactForm from 'components/modules/Contact/ContactForm';
 const Contact = () => (
   <div className="container py-16 flex items-start justify-between flex-col lg:flex-row" id="contact">
     <div className="flex-none w-full lg:w-auto lg:flex-1 pr-0 lg:pr-8 order-2 lg:order-1">
-      <h2 className="underline underline-offset-8 text-2xl md:text-4xl font-bold mb-8">Contact me</h2>
+      <h2 className="text-2xl md:text-4xl font-bold mb-1">Contact me</h2>
+      <span className="bg-black inline-block h-0.5 w-20 rounded mb-4" />
       <ContactForm />
     </div>
     <div className="flex-none w-full lg:w-auto lg:flex-1 order-1 lg:order-2 mb-8 lg:mb-0">
