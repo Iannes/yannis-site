b07eff34631e07ea3244eca04502601a451bb3f0
Yannis Spyrou <yannispyrou@gmail.com>
2022-10-25 14:27:51 +0200

    add categories

---
+++ src/components/modules/Categories/index.tsx
@@ -0,0 +1,31 @@
+type CategoryProps = {
+  category: string;
+};
+
+type CategoriesProps = {
+  categories: string[];
+};
+
+export const Categories: React.FC<CategoriesProps> = ({ categories }) => {
+  return categories ? (
+    <article className="py-4 xl:py-8">
+      <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400 mb-2">categories</h2>
+      <div className="flex flex-wrap">
+        {categories.map((category: string) => (
+          <Category key={category} category={category} />
+        ))}
+      </div>
+    </article>
+  ) : null;
+};
+
+const Category: React.FC<CategoryProps> = ({ category }) => {
+  return (
+    <p
+      className="mr-3 text-sm font-medium uppercase text-teal-500 hover:text-teal-600 dark:hover:text-teal-400"
+      key={category}
+    >
+      {category}
+    </p>
+  );
+};
--- src/components/ui/Posts/PostSidebar/index.tsx
+++ src/components/ui/Posts/PostSidebar/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import { NavigationControl } from './NavigationControl';
+import { Categories } from 'components/modules/Categories';
+import { NavigationControls } from './NavigationControls';
 import { Post } from 'utils/mdx-utils';
 
 export type PostSidebarProps = {
@@ -11,28 +12,8 @@ export type PostSidebarProps = {
 export const PostSidebar: React.FC<PostSidebarProps> = ({ categories, next, prev }) => {
   return (
     <aside className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
-      {categories && (
-        <article className="py-4 xl:py-8">
-          <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400 mb-2">categories</h2>
-          <div className="flex flex-wrap">
-            {categories.map((category: string) => (
-              // <Category key={category} text={category} />
-              <p
-                className="mr-3 text-sm font-medium uppercase text-teal-500 hover:text-teal-600 dark:hover:text-teal-400"
-                key={category}
-              >
-                {category}
-              </p>
-            ))}
-          </div>
-        </article>
-      )}
-      {(next || prev) && (
-        <nav className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
-          {prev && <NavigationControl postInfo={prev} text="Previous Article" />}
-          {next && <NavigationControl postInfo={next} text="Next Article" />}
-        </nav>
-      )}
+      <Categories categories={categories} />
+      <NavigationControls next={next} prev={prev} />
       <div className="pt-4 xl:pt-8 text-teal-500 hover:text-teal-600 dark:hover:text-teal-400">
         <Link href="/blog">&larr; Back to the blog</Link>
       </div>
