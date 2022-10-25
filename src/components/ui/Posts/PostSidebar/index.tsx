4f9ae1ff18cd793171685992cd292c864022344b
Yannis Spyrou <yannispyrou@gmail.com>
2022-10-25 14:19:36 +0200

    add nav and aside

--- src/components/ui/Posts/PostSidebar/index.tsx
+++ src/components/ui/Posts/PostSidebar/index.tsx
@@ -10,9 +10,9 @@ export type PostSidebarProps = {
 
 export const PostSidebar: React.FC<PostSidebarProps> = ({ categories, next, prev }) => {
   return (
-    <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
+    <aside className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
       {categories && (
-        <div className="py-4 xl:py-8">
+        <article className="py-4 xl:py-8">
           <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400 mb-2">categories</h2>
           <div className="flex flex-wrap">
             {categories.map((category: string) => (
@@ -25,17 +25,17 @@ export const PostSidebar: React.FC<PostSidebarProps> = ({ categories, next, prev
               </p>
             ))}
           </div>
-        </div>
+        </article>
       )}
       {(next || prev) && (
-        <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
+        <nav className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
           {prev && <NavigationControl postInfo={prev} text="Previous Article" />}
           {next && <NavigationControl postInfo={next} text="Next Article" />}
-        </div>
+        </nav>
       )}
       <div className="pt-4 xl:pt-8 text-teal-500 hover:text-teal-600 dark:hover:text-teal-400">
         <Link href="/blog">&larr; Back to the blog</Link>
       </div>
-    </div>
+    </aside>
   );
 };
